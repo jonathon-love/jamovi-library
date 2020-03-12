4fed689d08283654e0531e6cd3bb4ef69dbad7dd
Jonathon Love <jon@thon.cc>
2020-03-12 22:57:26 +1100

    1908 for linux

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install    --user -y flathub org.freedesktop.Platform//18.08
-flatpak install    --user -y flathub org.freedesktop.Sdk//18.08
+flatpak install    --user -y flathub org.freedesktop.Platform//19.08
+flatpak install    --user -y flathub org.freedesktop.Sdk//19.08
 
 if flatpak info --user org.jamovi.jamovi 1>/dev/null 2>/dev/null ; then
     echo "jamovi already installed"
 else
     echo "installing jamovi"
-    flatpak install    --user -y flathub org.jamovi.jamovi
+    until flatpak install --user -y https://dl.flathub.org/build-repo/15889/org.jamovi.jamovi.flatpakref; do echo "Trying again"; sleep 2; done
+    # flatpak install    --user -y flathub org.jamovi.jamovi
 fi
