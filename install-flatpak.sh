caa0f9711544cd2ba68dd9b9a64b6f1d8add474f
Jonathon Love <jon@thon.cc>
2021-10-14 12:11:22 +1100

    Updating flatpak install

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+dpkg-reconfigure ca-certificates
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 #if flatpak info --user org.jamovi.jamovi 1>/dev/null 2>/dev/null ; then
 #    echo "jamovi already installed"
 #else
     echo "installing jamovi"
-    # until flatpak install --user -y flathub org.jamovi.jamovi; do echo "Trying again"; sleep 2; done
-    until flatpak install --user -y https://dl.flathub.org/build-repo/59917/org.jamovi.jamovi.flatpakref; do echo "Trying again"; sleep 2; done
+    flatpak install --user -y flathub org.jamovi.jamovi
+    # until flatpak install --user -y https://dl.flathub.org/build-repo/59917/org.jamovi.jamovi.flatpakref; do echo "Trying again"; sleep 2; done
 #fi
 
 flatpak install    --user -y flathub org.freedesktop.Platform//21.08
